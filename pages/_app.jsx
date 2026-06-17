5dae58a7654d6f958d0d95ddedf9147b919bef7d
avijitdas-code <183139300+avijitdas-code@users.noreply.github.com>
2026-06-17 13:07:59 +0530

    Remove unused pages router

--- pages/_app.jsx
+++
@@ -1,8 +0,0 @@
-// pages/_app.jsx
-import '../app/globals.css'; // Points correctly to your app folder
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
